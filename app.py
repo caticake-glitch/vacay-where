47428c1ace0e428f5bf8aacdd25185c8a10f916a
caticake-glitch <diabolicacattus@gmail.com>
2026-09-13 21:29:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["10 per hour"],
+    default_limits=["300 per hour"],
     storage_uri="memory://",
     strategy="fixed-window",
 )
